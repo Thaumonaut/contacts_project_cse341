f469d38646dd9d538d4be313d52a94dcf7b5ce79
Jacob Perry <perryjay123@live.com>
2024-09-20 19:04:15 -0700

    fixed lingering /api/contacts in strings

--- routes/HomeRoutes.js
+++ routes/HomeRoutes.js
@@ -4,7 +4,7 @@ const route = express.Router();
 const mongodb = require('../data/database')
 
 route.get('/', (req, res) => {
-  res.send('API Home route. Go to /api/contacts to see all contacts.')
+  res.send('API Home route. Go to /contacts to see all contacts.')
 })
 
 module.exports = route;
